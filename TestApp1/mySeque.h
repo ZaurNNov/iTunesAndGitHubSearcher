192e22541c2b90f443042bfd726109f3e7a6bc8e
Zaur Giyasov <zagiyas@mera.ru>
2018-05-24 17:49:27 +0300

    3

---
+++ TestApp1/mySeque.h
@@ -0,0 +1,13 @@
+//
+//  mySeque.h
+//  TestApp1
+//
+//  Created by Zaur Giyasov on 24/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mySeque : UIStoryboardSegue
+
+@end
---
+++ TestApp1/mySeque.m
@@ -0,0 +1,13 @@
+//
+//  mySeque.m
+//  TestApp1
+//
+//  Created by Zaur Giyasov on 24/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import "mySeque.h"
+
+@implementation mySeque
+
+@end
